8cfc0ce6080a352c91cde1cd6533343af24174ac
Subhi YD <subhy809fr@gmail.com>
2022-09-30 12:59:01 +0200

    2ème commit: 1.infos to the browser from responses, req.url

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,2 +1,29 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    // console.log(req.method);
+    // console.log(req.url);
+    const url = req.url;
 
+    if( url === '/') {
+        res.writeHead(200, {'content-type':'text/html'}) // 'content-type':'text/plain'
+        res.write('<h1>Home Page</h1>');
+        res.end();
+        // same as res.end('<h1>Home Page</h1>'); 
+    }
+    else if( url === '/about') {
+        res.writeHead(200, {'content-type':'text/html'}) // 'content-type':'text/plain'
+        res.write('<h1>About Page !</h1>');
+        res.end();
+        // same as res.end('<h1>About Page !</h1>'); 
+    }
+    else {
+        res.writeHead(404, {'content-type':'text/html'}) // 'content-type':'text/plain'
+        res.write('<h1>page not found ! </h1>');
+        res.end();
+        // same as res.end('<h1>page not found !</h1>'); 
+    }
+
+ 
+});
+server.listen(5000);
 
