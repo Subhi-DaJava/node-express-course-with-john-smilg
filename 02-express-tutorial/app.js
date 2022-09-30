154acd9bbe26cccb98c99748dfc43c3a64062750
Subhi YD <subhy809fr@gmail.com>
2022-09-30 18:31:31 +0200

    3ème commit: add index.html for testing only as a file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,4 +1,11 @@
 const http = require('http');
+const {readFileSync} = require('fs');
+
+// get all files 
+const homePage = readFileSync('./index.html'); // some how static on init
+
+
+
 const server = http.createServer((req, res) => {
     // console.log(req.method);
     // console.log(req.url);
@@ -6,7 +13,7 @@ const server = http.createServer((req, res) => {
 
     if( url === '/') {
         res.writeHead(200, {'content-type':'text/html'}) // 'content-type':'text/plain'
-        res.write('<h1>Home Page</h1>');
+        res.write(homePage);
         res.end();
         // same as res.end('<h1>Home Page</h1>'); 
     }
